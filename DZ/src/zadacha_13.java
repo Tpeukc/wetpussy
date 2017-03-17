9dd2cdc6dedbacbd7fb285e1d148e957bf241cfd
Tpeukc <micvajers@gmail.com>
2017-03-17 12:01:29 +0300

    updt 0.2 - add DZ 170315 - add day 2 work

--- DZ/src/zadacha_13.java
+++ DZ/src/zadacha_13.java
@@ -1,5 +1,25 @@
 /**
  * Created by YurievAP on 17.03.2017.
  */
-public class zadacha_13 {
+import  java.util.*;
+public class zadacha_13{
+    public static void main(String[] args){
+        int a;
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Введите натуральное число");
+
+        if(scn.hasNextInt()){
+
+            a=scn.nextInt();
+
+            System.out.print("Делителями числа "+a+" являются ");
+            for(int i=a;i>0;i--){
+                int b=a%i;
+                if(b==0)
+                    System.out.print(i+" ");
+            }
+        }
+        else System.out.println("Ошибка. Введено не число");
+
+    }
 }
